9b7cd10d83abb5b182e76f6fe9eae6100ffcba5b
Jordan Adams <jordan_callum@live.co.uk>
2011-08-27 23:13:02 +0100

    Bugfix: Overlay size and clone position not refreshing upon window resize.

--- jquery.dimlights.js
+++ jquery.dimlights.js
@@ -8,6 +8,10 @@
 
 	var screen_width = $(window).width();
 	var screen_height = $(window).height();
+
+	var o_left;
+	var o_top;
+
 	var clone;
 	var settings;
 
@@ -29,8 +33,20 @@
 		$('#dtl_overlay').fadeTo(200, settings.opacity);
 
 		// get element position
-		var o_left = this.offset().left;
-		var o_top = this.offset().top;
+		o_left = this.offset().left;
+		o_top = this.offset().top;
+
+		target = this;
+
+		$(window).bind('resize', function() {
+			
+			o_left = target.offset().left;
+			o_top = target.offset().top;
+			
+			clone.css('top', o_top);
+			clone.css('left', o_left);		
+
+		});
 
 		// clone the target element
 		clone = this.clone();
@@ -60,13 +76,24 @@
 
 	};
 
-
+	// Bind 'close on click' to overlay
 	$('#dtl_overlay').live('click', function() {
 		
 		$(this).fadeOut(200, function() {
 			$(this).detach();
 			clone.detach();
 		});
+
+	});
+
+	// Keep overlay size correct
+	$(window).bind('resize', function() {
+
+		screen_width = $(window).width();
+		screen_height = $(window).height();
+
+		$('#dtl_overlay').width(screen_width);
+		$('#dtl_overlay').height(screen_height);
 
 	});
 
