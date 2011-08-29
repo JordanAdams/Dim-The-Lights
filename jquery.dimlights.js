7e89b7b5172fa4ba6a475743a0173a9459a8783f
Jordan Adams <jordan_callum@live.co.uk>
2011-08-29 16:37:25 +0100

    softBg now defaults to false

--- jquery.dimlights.js
+++ jquery.dimlights.js
@@ -1,9 +1,10 @@
 (function($) {
 
 	var defaults = {	
-		opacity : 0.85,
-		classes : [],
-		softBg  : true,
+		opacity    : 0.85,
+		classes    : [],
+		softBg     : false,
+		hideTarget : true,
 
 		// background: '',
 	}
@@ -14,14 +15,15 @@
 	var o_top;
 	var clone;
 	var settings;
-
+	var target;
 
 	$.fn.dimTheLights = function(options) {
 		
 		// merge options into settings
 		settings = $.extend({}, defaults, options);
 
-		console.log(settings);
+		// reference target (jquery fucks with 'this')
+		target = this;
 
 		// add overlay to DOM
 		$('body').append('<div id="dtl_overlay"></div>');
@@ -36,8 +38,6 @@
 		// get element position
 		o_left = this.offset().left;
 		o_top = this.offset().top;
-
-		target = this;
 
 		$(window).bind('resize', function() {
 			
@@ -87,27 +87,30 @@
 			clone.addClass(settings.classes[c]);
 		}
 
-	};
+		// hide target?
+		settings.hideTarget ? target.css('visibility', 'hidden') : false;
 
-	// Bind 'close on click' to overlay
-	$('#dtl_overlay').live('click', function() {
-		
-		$(this).fadeOut(200, function() {
-			$(this).detach();
-			clone.detach();
+		// Bind 'close on click' to overlay
+		$('#dtl_overlay').live('click', function() {
+			
+			$(this).fadeOut(200, function() {
+				$(this).detach();
+				clone.detach();
+			});
+
 		});
 
-	});
+		// Keep overlay size correct
+		$(window).bind('resize', function() {
 
-	// Keep overlay size correct
-	$(window).bind('resize', function() {
+			screen_width = $(window).width();
+			screen_height = $(window).height();
 
-		screen_width = $(window).width();
-		screen_height = $(window).height();
+			$('#dtl_overlay').width(screen_width);
+			$('#dtl_overlay').height(screen_height);
 
-		$('#dtl_overlay').width(screen_width);
-		$('#dtl_overlay').height(screen_height);
+		});
 
-	});
+	};
 
 })(jQuery);
